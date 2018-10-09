80eb7dad8b32ede05bd2cc5ceee5df4b3e3d8180
Ivan Smetanin <smetanin23@yandex.ru>
2018-10-09 18:21:40 +0300

    Add example, fix access level

--- Example/MailyExample/ViewController.swift
+++ Example/MailyExample/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import Maily
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var recipientTextField: UITextField!
+    @IBOutlet weak var subjectTextField: UITextField!
+    @IBOutlet weak var bodyTextField: UITextField!
+
+    // MARK: - IBActions
+
+    @IBAction func sendButtonAction(_ sender: UIButton) {
+        sendEmail()
     }
 
+    // MARK: - UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Private methods
+
+    private func sendEmail() {
+        if Maily.canSendMail {
+            Maily.sendMail(
+                recipient: recipientTextField.text, subject: subjectTextField.text, body: bodyTextField.text,
+                onCompleted: {
+                    print("completed")
+                },
+                onCancel: {
+                    print("cancel")
+                }
+            )
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Can't find any email clients", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 
 }
 
